b78e4ec02dcb14a89aecbe2909cc40b1c58d1ca5
Chioma-Opara <157995126+Chioma-Opara@users.noreply.github.com>
2024-04-14 19:20:36 -0400

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -54,8 +54,14 @@ public class Engine {
      * @return true if the engine has enough fuel and false if not
      */
     public boolean go(){
-        if (this.currentFuel > 0){
-            setcurrentFuel(currentFuel - 5);;
+        if (this.currentFuel > 5){
+            setcurrentFuel(currentFuel - 5);
+            System.out.print.ln("Your current fuel level is: ", this.currentFuel);
+            return true;
+        }
+        else if (this.currentFuel > 0 && this.currentFuel < 5){
+            setcurrentFuel(currentFuel - 1);
+            System.out.print.ln("You are running low on fuel! Your current fuel level is: ", this.currentFuel);
             return true;
         }
         else{
